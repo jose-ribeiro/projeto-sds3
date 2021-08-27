07ec268ec1b24d8cb1742065124dd6d85eedae27
jose-ribeiro <57302604+jose-ribeiro@users.noreply.github.com>
2021-08-27 09:20:13 -0300

    Graficos com dados Staticos

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,31 @@
 import Navbar from "components/Navbar";
 import Footer from "components/Footer";
 import DataTable from "components/DataTable";
+import BarChart from "components/BarChart";
+import DonutChart from "components/DonutChart";
+
 function App() {
   return (
     <>
       <Navbar />
       <div className="container">
-        <h1 className="text-primary">Olá mundo!!</h1>
+        <h1 className="text-primary py-3">Painel de vendas</h1>
 
+        <div className="row px-3">
+            <div className="col-sm-6">
+              <h5 className='text-center text-secondary'>Taxa de Sucesso</h5>
+             <BarChart />
+            </div>
+           <div className="col-sm-6">
+            <h5 className='text-center text-secondary'>Taxa de sucesso</h5>
+            <DonutChart />
+           </div>
+          </div>
+
+          <div className="py-3">
+            <h2 className="text-primary">Todas as vendas</h2>
+          </div>
+        
         <DataTable />
       </div>
       <Footer />
