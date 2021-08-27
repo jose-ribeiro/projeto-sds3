d724c61ea36fb424dfee4bc1b2f17fc9fdd460a3
jose-ribeiro <57302604+jose-ribeiro@users.noreply.github.com>
2021-08-27 08:27:33 -0300

    Componentes Basicos Staticos

---
+++ frontend/src/components/DataTable/index.tsx
@@ -0,0 +1,78 @@
+function DataTable() {
+    return (
+        <div className="table-responsive">
+    <table className="table table-striped table-sm">
+        <thead>
+            <tr>
+                <th>Data</th>
+                <th>Vendedor</th>
+                <th>Clientes visitados</th>
+                <th>Negócios fechados</th>
+                <th>Valor</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>22/04/2021</td>
+                <td>Barry Allen</td>
+                <td>34</td>
+                <td>25</td>
+                <td>15017.00</td>
+            </tr>
+            <tr>
+                <td>22/04/2021</td>
+                <td>Jose Willian</td>
+                <td>14</td>
+                <td>20</td>
+                <td>1100.00</td>
+            </tr>
+            <tr>
+                <td>22/04/2021</td>
+                <td>Carlos augusto</td>
+                <td>22</td>
+                <td>14</td>
+                <td>900.00</td>
+            </tr>
+            <tr>
+                <td>22/04/2021</td>
+                <td>Barry Allen</td>
+                <td>34</td>
+                <td>25</td>
+                <td>15017.00</td>
+            </tr>
+            <tr>
+                <td>22/04/2021</td>
+                <td>Barry Allen</td>
+                <td>34</td>
+                <td>25</td>
+                <td>15017.00</td>
+            </tr>
+            <tr>
+                <td>22/04/2021</td>
+                <td>Barry Allen</td>
+                <td>34</td>
+                <td>25</td>
+                <td>15017.00</td>
+            </tr>
+            <tr>
+                <td>22/04/2021</td>
+                <td>Barry Allen</td>
+                <td>34</td>
+                <td>25</td>
+                <td>15017.00</td>
+            </tr>
+            <tr>
+                <td>22/04/2021</td>
+                <td>Barry Allen</td>
+                <td>34</td>
+                <td>25</td>
+                <td>15017.00</td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+
+    );
+}
+
+export default DataTable;
