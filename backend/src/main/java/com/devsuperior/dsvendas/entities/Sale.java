27370f000dd4502eff3fc8777d3523585ed75905
jose-ribeiro <57302604+jose-ribeiro@users.noreply.github.com>
2021-08-30 08:29:14 -0300

    Correção de coneção com banco de dados

--- backend/src/main/java/com/devsuperior/dsvendas/entities/Sale.java
+++ backend/src/main/java/com/devsuperior/dsvendas/entities/Sale.java
@@ -1,83 +1,88 @@
 package com.devsuperior.dsvendas.entities;
 
-import javax.persistence.*;
 import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "tb_sales")
 public class Sale {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private Integer visited;
+	private Integer deals;
+	private Double amount;
+	private LocalDate date;
+	
+	@ManyToOne
+	@JoinColumn(name = "seller_id")
+	private Seller seller;
+	
+	public Sale() {
+	}
 
-    private  Integer visited;
-    private Integer deals;
-    private Double amount;
-    private LocalDate date;
+	public Sale(Long id, Integer visited, Integer deals, Double amount, LocalDate date, Seller seller) {
+		this.id = id;
+		this.visited = visited;
+		this.deals = deals;
+		this.amount = amount;
+		this.date = date;
+		this.seller = seller;
+	}
 
-    @ManyToOne
-    @JoinColumn(name = "seller_id")
-    private Seller seller;
+	public Long getId() {
+		return id;
+	}
 
-    public Sale(){
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    }
+	public Integer getVisited() {
+		return visited;
+	}
 
-    public Sale(Long id, Integer visited, Integer deals, Double amount, LocalDate date, Seller seller) {
-        this.id = id;
-        this.visited = visited;
-        this.deals = deals;
-        this.amount = amount;
-        this.date = date;
-        this.seller = seller;
-    }
+	public void setVisited(Integer visited) {
+		this.visited = visited;
+	}
 
-    public Long getId() {
-        return id;
-    }
+	public Integer getDeals() {
+		return deals;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public void setDeals(Integer deals) {
+		this.deals = deals;
+	}
 
-    public Integer getVisited() {
-        return visited;
-    }
+	public Double getAmount() {
+		return amount;
+	}
 
-    public void setVisited(Integer visited) {
-        this.visited = visited;
-    }
+	public void setAmount(Double amount) {
+		this.amount = amount;
+	}
 
-    public Integer getDeals() {
-        return deals;
-    }
+	public LocalDate getDate() {
+		return date;
+	}
 
-    public void setDeals(Integer deals) {
-        this.deals = deals;
-    }
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
 
-    public Double getAmount() {
-        return amount;
-    }
+	public Seller getSeller() {
+		return seller;
+	}
 
-    public void setAmount(Double amount) {
-        this.amount = amount;
-    }
-
-    public LocalDate getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDate date) {
-        this.date = date;
-    }
-
-    public Seller getSeller() {
-        return seller;
-    }
-
-    public void setSeller(Seller seller) {
-        this.seller = seller;
-    }
+	public void setSeller(Seller seller) {
+		this.seller = seller;
+	}
 }
