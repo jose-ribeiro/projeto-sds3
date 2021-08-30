b9f5f38a5dd56b9b4b3fd11d9cc8f0207dff19ad
jose-ribeiro <57302604+jose-ribeiro@users.noreply.github.com>
2021-08-30 19:51:38 -0300

    Paginação

---
+++ backend/src/main/java/com/devsuperior/dsvendas/service/SaleService.java
@@ -0,0 +1,24 @@
+package com.devsuperior.dsvendas.service;
+
+import com.devsuperior.dsvendas.dto.SaleDTO;
+
+import com.devsuperior.dsvendas.entities.Sale;
+import com.devsuperior.dsvendas.repositories.SaleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class SaleService {
+    @Autowired
+    private SaleRepository repository;
+
+    public Page<SaleDTO> findAll (Pageable pegeable){
+        Page<Sale> result = repository.findAll(pegeable);
+        return result.map(x -> new SaleDTO(x));
+    }
+}
+
+
